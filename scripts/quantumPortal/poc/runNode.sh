f13c64de8b5d7cd6f633f99ffdcba8b0ef5229fd
3ierratango <118272976+3ierratango@users.noreply.github.com>
2022-11-22 15:58:18 +0400

    fix runNode

--- scripts/quantumPortal/poc/runNode.sh
+++ scripts/quantumPortal/poc/runNode.sh
@@ -4,6 +4,5 @@ echo node running...
 while [ true ]
 do
   sleep 10
-  npx hardhat run ./scripts/quantumPortal/poc/runNode.ts --network rinkeby
+  npx hardhat run ./scripts/quantumPortal/poc/runNode.ts --network bsctestnet
 done
-
