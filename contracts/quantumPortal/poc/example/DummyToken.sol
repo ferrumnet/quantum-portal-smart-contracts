9a8a8f0eb31587db245f756164444cf54e683838
sibghatullah1997 <h.m.sibghatullah1997@gmail.com>
2022-11-29 13:33:00 +0500

    conflict resolved

--- contracts/quantumPortal/poc/example/DummyToken.sol
+++ contracts/quantumPortal/poc/example/DummyToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "../../../common/IFerrumDeployer.sol";
+import "foundary-contracts/contracts/common/IFerrumDeployer.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DummyToken is ERC20Burnable {
