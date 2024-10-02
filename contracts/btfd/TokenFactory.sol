6e1bbb298e15454e6562cfc3dfc467b54f1902b0
Naiem Yeganeh <naiem@ferrum.network>
2024-10-02 18:11:43 +0300

    Make TokenFactory for btfd upgradeable

--- contracts/btfd/TokenFactory.sol
+++ contracts/btfd/TokenFactory.sol
@@ -5,6 +5,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Create2.sol";
 import "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
 import "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+import {UUPSUpgradeable, Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {IVersioned} from "foundry-contracts/contracts/contracts/common/IVersioned.sol";
 import "./Bitcoin.sol";
 import "./QpErc20Token.sol";
 import "./WalletRegistration.sol";
@@ -14,50 +17,133 @@ import "hardhat/console.sol";
 
 error OnlyQpToken();
 
-contract TokenFactory is ITokenFactory, Ownable {
-    address public runeImplementation;
-    UpgradeableBeacon public runeBeacon;
-    address public btcImplementation;
-    UpgradeableBeacon public btcBeacon;
-    address public btc;
-    address public override portal;
-    address public override feeConvertor; // Has the price for fee VS native token
-    address public override feeStore; // Stores fee for rune transactions
-    address public override qpWallet;
-    address public override qpRuneWallet;
-    address public override registration;
-    mapping (bytes32 => address) public runeTokens;
-    mapping (address => bytes32) public runeTokensByAddress;
+contract TokenFactory is
+    ITokenFactory,
+    Initializable,
+    UUPSUpgradeable,
+    OwnableUpgradeable,
+    IVersioned {
+    string public constant override VERSION = "000.001";
+    struct TokenFactoryStorageV001 {
+        address runeImplementation;
+        UpgradeableBeacon runeBeacon;
+        address btcImplementation;
+        UpgradeableBeacon btcBeacon;
+        address btc;
+        address portal;
+        address feeConvertor; // Has the price for fee VS native token
+        address feeStore; // Stores fee for rune transactions
+        address qpWallet;
+        address qpRuneWallet;
+        address registration;
+        mapping (bytes32 => address) runeTokens;
+        mapping (address => bytes32) runeTokensByAddress;
+    }
 
+    // keccak256(abi.encode(uint256(keccak256("ferrum.storage.TokenFactory.001")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 private constant TokenFactoryStorageV001Location = 0x86b1ac319d039482847cca0b2f4f5ab27bfdb4a12864324807f7819c61b01d00;
+
+    function _getTokenFactoryStorageV001() internal pure returns (TokenFactoryStorageV001 storage $) {
+        assembly {
+            $.slot := TokenFactoryStorageV001Location
+        }
+    }
+
+    function initialize(address _portal, address _feeConvertor, address _qpWallet, address _qpRuneWallet, address initialOwner
+    ) public initializer {
+        __TokenFactory_init(_portal, _feeConvertor, _qpWallet, _qpRuneWallet);
+        __Ownable_init(initialOwner);
+    }
+
+    function __TokenFactory_init(address _portal, address _feeConvertor, address _qpWallet, address _qpRuneWallet
+    ) internal onlyInitializing {
+        TokenFactoryStorageV001 storage $ = _getTokenFactoryStorageV001();
+        $.runeImplementation = address(new QpErc20Token{salt: bytes32(0x0)}());
+        $.runeBeacon = new UpgradeableBeacon{salt: bytes32(uint256(0x1))}($.runeImplementation, address(this));
+        $.btcImplementation = address(new Bitcoin{salt: bytes32(0x0)}());
+        $.btcBeacon = new UpgradeableBeacon{salt: bytes32(uint256(0x2))}($.btcImplementation, address(this));
+        deployBitcoin();
+        $.registration = address(new WalletRegistration{salt: bytes32(0x0)}());
+        $.feeStore = address(new FeeStore{salt: bytes32(0x0)}());
+        $.portal = _portal;
+        $.feeConvertor = _feeConvertor;
+        $.qpWallet = _qpWallet;
+        $.qpWallet = _qpRuneWallet;
+    }
+
+    function __TokenFactory_init_unchained() internal onlyInitializing {}
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+    
     event Deployed(address impl, address deped, address beacon);
 
-    constructor(address _portal, address _feeConvertor, address _qpWallet, address _qpRuneWallet) Ownable(msg.sender) {
-        runeImplementation = address(new QpErc20Token{salt: bytes32(0x0)}());
-        runeBeacon = new UpgradeableBeacon{salt: bytes32(uint256(0x1))}(runeImplementation, address(this));
-        btcImplementation = address(new Bitcoin{salt: bytes32(0x0)}());
-        btcBeacon = new UpgradeableBeacon{salt: bytes32(uint256(0x2))}(btcImplementation, address(this));
-        deployBitcoin();
-        registration = address(new WalletRegistration{salt: bytes32(0x0)}());
-        feeStore = address(new FeeStore{salt: bytes32(0x0)}());
-        portal = _portal;
-        feeConvertor = _feeConvertor;
-        qpWallet = _qpWallet;
-        qpWallet = _qpRuneWallet;
+    function runeImplementation() external view returns (address) {
+        return _getTokenFactoryStorageV001().runeImplementation;
+    }
+
+    function runeBeacon() external view returns (address) {
+        return address(_getTokenFactoryStorageV001().runeBeacon);
+    }
+
+    function btcImplementation() external view returns (address) {
+        return _getTokenFactoryStorageV001().btcImplementation;
+    }
+
+    function btcBeacon() external view returns (address) {
+        return address(_getTokenFactoryStorageV001().btcBeacon);
+    }
+
+    function btc() external view returns (address) {
+        return _getTokenFactoryStorageV001().btc;
+    }
+
+    function portal() external override view returns (address) {
+        return _getTokenFactoryStorageV001().portal;
+    }
+
+    function feeConvertor() external override view returns (address) {
+        return _getTokenFactoryStorageV001().feeConvertor;
+    }
+
+    function feeStore() external override view returns (address) {
+        return _getTokenFactoryStorageV001().feeStore;
+    }
+
+    function qpWallet() external override view returns (address) {
+        return _getTokenFactoryStorageV001().qpWallet;
+    }
+
+    function qpRuneWallet() external override view returns (address) {
+        return _getTokenFactoryStorageV001().qpRuneWallet;
+    }
+
+    function registration() external override view returns (address) {
+        return _getTokenFactoryStorageV001().registration;
+    }
+
+    function runeTokens(bytes32 salt) external view returns (address) {
+        return _getTokenFactoryStorageV001().runeTokens[salt];
+    }
+
+    function runeTokensByAddress(address addr) external view returns (bytes32) {
+        return _getTokenFactoryStorageV001().runeTokensByAddress[addr];
     }
 
     function updatePortal(address _portal, address _feeConvertor, address _qpWallet, address _qpRuneWallet) external onlyOwner {
-        portal = _portal;
-        feeConvertor = _feeConvertor;
-        qpWallet = _qpWallet;
-        qpWallet = _qpRuneWallet;
+        TokenFactoryStorageV001 storage $ = _getTokenFactoryStorageV001();
+        $.portal = _portal;
+        $.feeConvertor = _feeConvertor;
+        $.qpWallet = _qpWallet;
+        $.qpWallet = _qpRuneWallet;
     }
 
     function upgradeImplementations(address newRuneImpl, address newBtcImpl, address _registration) external onlyOwner {
-        runeBeacon.upgradeTo(newRuneImpl);
-        runeImplementation = newRuneImpl;
-        btcBeacon.upgradeTo(newBtcImpl);
-        btcImplementation = newBtcImpl;
-        registration = _registration;
+        TokenFactoryStorageV001 storage $ = _getTokenFactoryStorageV001();
+        $.runeBeacon.upgradeTo(newRuneImpl);
+        $.runeImplementation = newRuneImpl;
+        $.btcBeacon.upgradeTo(newBtcImpl);
+        $.btcImplementation = newBtcImpl;
+        $.registration = _registration;
     }
 
     function getRuneTokenAddress(
@@ -65,7 +151,8 @@ contract TokenFactory is ITokenFactory, Ownable {
         uint version
     ) external view returns(address) {
         bytes32 salt = keccak256(abi.encode(runeId, version));
-        return runeTokens[salt];
+        TokenFactoryStorageV001 storage $ = _getTokenFactoryStorageV001();
+        return $.runeTokens[salt];
     }
 
     function deployRuneToken(
@@ -81,8 +168,9 @@ contract TokenFactory is ITokenFactory, Ownable {
         // then init(...)
         // So that no one can deploy the token with invalid name/decimals, etc.
         // The token's init function must validate the metadata (name, symbol, etc)
+        TokenFactoryStorageV001 storage $ = _getTokenFactoryStorageV001();
         bytes32 salt = keccak256(abi.encode(runeId, version));
-        BeaconProxy dep = new BeaconProxy{salt: salt}(address(runeBeacon), new bytes(0));
+        BeaconProxy dep = new BeaconProxy{salt: salt}(address($.runeBeacon), new bytes(0));
         QpErc20Token(address(dep)).initialize(
             runeId,
             version,
@@ -92,9 +180,10 @@ contract TokenFactory is ITokenFactory, Ownable {
             totalSupply,
             deployTxId
         );
-        runeTokens[salt] = address(dep);
-        runeTokensByAddress[address(dep)] = salt;
-        emit Deployed(runeImplementation, address(dep), address(runeBeacon));
+        
+        $.runeTokens[salt] = address(dep);
+        $.runeTokensByAddress[address(dep)] = salt;
+        emit Deployed($.runeImplementation, address(dep), address($.runeBeacon));
     }
 
     /**
@@ -102,17 +191,20 @@ contract TokenFactory is ITokenFactory, Ownable {
      */
     function feeStoreCollectFee(
         bytes32 txId) external returns (uint) {
-        if (runeTokensByAddress[msg.sender] == 0x0) { revert OnlyQpToken(); }
-        return FeeStore(feeStore).collectFee(txId);
+        TokenFactoryStorageV001 storage $ = _getTokenFactoryStorageV001();
+        if ($.runeTokensByAddress[msg.sender] == 0x0) { revert OnlyQpToken(); }
+        return FeeStore($.feeStore).collectFee(txId);
     }
 
     function feeStoreSweepToken(address token, uint amount, address to) external override {
-        FeeStore(feeStore).sweepToken(token, amount, to);
+        TokenFactoryStorageV001 storage $ = _getTokenFactoryStorageV001();
+        FeeStore($.feeStore).sweepToken(token, amount, to);
     }
 
     function deployBitcoin() internal {
-        btc = address(new BeaconProxy{salt: bytes32(uint256(0x11011))}(address(btcBeacon), new bytes(0)));
-        runeTokensByAddress[address(btc)] = bytes32(uint256(0x11011));
-        Bitcoin(btc).initialize();
+        TokenFactoryStorageV001 storage $ = _getTokenFactoryStorageV001();
+        $.btc = address(new BeaconProxy{salt: bytes32(uint256(0x11011))}(address($.btcBeacon), new bytes(0)));
+        $.runeTokensByAddress[address($.btc)] = bytes32(uint256(0x11011));
+        Bitcoin($.btc).initialize();
     }
 }
--- test/btfd/TestDeployBtfd.ts
+++ test/btfd/TestDeployBtfd.ts
@@ -1,14 +1,15 @@
 import { ethers } from "hardhat";
 import { Wei, ZeroAddress } from "../common/Utils";
-import { TokenFactory } from '../../typechain-types/TokenFactory';
-import { QpErc20Token } from '../../typechain-types/QpErc20Token';
-import { Bitcoin } from '../../typechain-types/Bitcoin';
+import { Bitcoin, QpErc20Token, TokenFactory } from '../../typechain-types';
 import { randomBytes } from "crypto";
 
 describe("Deploy ", function () {
 	it('Deploy a bunch of tokens', async function() {
         const facF = await ethers.getContractFactory('TokenFactory');
-        const fac = await facF.deploy(ZeroAddress, ZeroAddress) as TokenFactory;
+        const fac = await facF.deploy() as any as TokenFactory;
+        const owner = (await ethers.getSigners())[0];
+        await fac.initialize(ZeroAddress, ZeroAddress, ZeroAddress, ZeroAddress, owner.address);
+        console.log(`runeImplementation: `);
 
         // Make sure the implementations are there
         console.log(`runeImplementation: ${await fac.runeImplementation()}`);
@@ -28,7 +29,7 @@ describe("Deploy ", function () {
         console.log(`Code: ${code}`);
 
         const qpTokF = await ethers.getContractFactory('QpErc20Token');
-        const qpTok = await qpTokF.attach(justDeployed) as QpErc20Token;
+        const qpTok = await qpTokF.attach(justDeployed) as any as QpErc20Token;
         console.log(`Token name: ${await qpTok.name()}`);
 
         const btc = await qpTok.attach(await fac.btc()) as QpErc20Token;
@@ -37,24 +38,28 @@ describe("Deploy ", function () {
 
     it('Can upgrade tokens', async function() {    
         const facF = await ethers.getContractFactory('TokenFactory');
-        const fac = await facF.deploy(ZeroAddress, ZeroAddress) as TokenFactory;
+        const fac = await facF.deploy() as any as TokenFactory;
+        const owner = (await ethers.getSigners())[0];
+        await fac.initialize(ZeroAddress, ZeroAddress, ZeroAddress, ZeroAddress, owner)
 
         const btcF = await ethers.getContractFactory('Bitcoin');
-        const btc = await btcF.attach(await fac.btc()) as Bitcoin;
+        const btc = await btcF.attach(await fac.btc()) as any as Bitcoin;
         console.log(`BTC ${await fac.btcImplementation()} name: ${await btc.name()} (${await btc.symbol()})`);
 
         // Deploy dummyUpgradedToken
         const dummyF = await ethers.getContractFactory('DummyUpgradedToken');
-        const dummy = await dummyF.deploy() as QpErc20Token;
+        const dummy = await dummyF.deploy() as any as QpErc20Token;
 
         // Now upgrade the token to a new implementation
-        await fac.upgradeImplementations(await fac.runeImplementation(), dummy.address, ZeroAddress);
+        await fac.upgradeImplementations(await fac.runeImplementation(), dummy.target, ZeroAddress);
         console.log(`BTC ${await fac.btcImplementation()} name: ${await btc.name()} (${await btc.symbol()})`);
     });
 
     it('Upgrade many at once', async function() {
         const facF = await ethers.getContractFactory('TokenFactory');
-        const fac = await facF.deploy(ZeroAddress, ZeroAddress) as TokenFactory;
+        const fac = await facF.deploy() as any as TokenFactory;
+        const owner = (await ethers.getSigners())[0];
+        await fac.initialize(ZeroAddress, ZeroAddress, ZeroAddress, ZeroAddress, owner)
         const tokIds = ['101', '102', '103', '104', '105'];
 
         console.log('Deploying tokens');
@@ -66,20 +71,20 @@ describe("Deploy ", function () {
         console.log('Before upgrade');
         for(const tokId of tokIds) {
             const tok = await fac.getRuneTokenAddress(parseInt(tokId), 1);
-            const rune = await ethers.getContractAt('QpErc20Token', tok) as QpErc20Token;
+            const rune = await ethers.getContractAt('QpErc20Token', tok) as any as QpErc20Token;
             console.log(`RUNE name: ${await rune.name()} (${await rune.symbol()})`);
         }
 
         // Deploy dummyUpgradedToken
         const dummyF = await ethers.getContractFactory('DummyUpgradedToken');
-        const dummy = await dummyF.deploy() as QpErc20Token;
+        const dummy = await dummyF.deploy() as any as QpErc20Token;
         console.log('Upgrading tokens');
-        await fac.upgradeImplementations(dummy.address, await fac.btcImplementation(), ZeroAddress);
+        await fac.upgradeImplementations(dummy.target, await fac.btcImplementation(), ZeroAddress);
 
         console.log('Check upgraded tokens');
         for(const tokId of tokIds) {
             const tok = await fac.getRuneTokenAddress(parseInt(tokId), 1);
-            const rune = await ethers.getContractAt('QpErc20Token', tok) as QpErc20Token;
+            const rune = await ethers.getContractAt('QpErc20Token', tok) as any as QpErc20Token;
             console.log(`RUNE name: ${await rune.name()} (${await rune.symbol()})`);
         }
     });
